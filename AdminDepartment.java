6ecb23ad2eb448c9fa3a8564ae2ba7603d69b8af
Sai Kiran Malla <116140132+skmrocks@users.noreply.github.com>
2022-12-18 23:39:11 +0530

    Adding comments and sample output
    Adding comments and sample output for all files.

--- AdminDepartment.java
+++ AdminDepartment.java
@@ -1,20 +1,41 @@
+/*----------------------------------------------------------------------------------------
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
 package com.group.gradedassignments.assignment01;
 
+/**
+ * AdminDepartment class for adding admin department functionalities.
+ * @see SuperDepartment admin department extends super department.
+ */
 public class AdminDepartment extends SuperDepartment {
+
     private static final String DEPARTMENTNAME = "Admin Department";
     private static final String TODAYSWORK = "Complete your documents Submission";
     private static final String WORKDEADLINE = "Complete by EOD";
 
+    /**
+     * Get method for department name.
+     * @return department name.
+     */
     @Override
     public String departmentName() {
         return DEPARTMENTNAME;
     }
 
+    /**
+     * Get method for today`s work.
+     * @return today`s work.
+     */
     @Override
     public String getTodaysWork() {
         return TODAYSWORK;
     }
 
+    /**
+     * Get method for work deadline.
+     * @return work deadline.
+     */
     @Override
     public String getWorkDeadline() {
         return WORKDEADLINE;
--- Driver.java
+++ Driver.java
@@ -1,13 +1,27 @@
+/*----------------------------------------------------------------------------------------
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
 package com.group.gradedassignments.assignment01;
 
+/**
+ * Driver class for printing each department functionalities.
+ */
 public class Driver {
+    
+    /**
+     * Main method for running the driver application to perform required steps to execute print functionality.
+     * @param args arguments for accepting parameters as string array.
+     */
     public static void main(String[] args) {
+
         AdminDepartment adminDepartment = new AdminDepartment();
         doPrintWelcomeMessage(adminDepartment.departmentName());
         doPrintMessage(adminDepartment.getTodaysWork());
         doPrintMessage(adminDepartment.getWorkDeadline());
         doPrintMessage(adminDepartment.isTodayAHoliday());
         doPrintMessage("");
+
         HrDepartment hrDepartment = new HrDepartment();
         doPrintWelcomeMessage(hrDepartment.departmentName());
         doPrintMessage(hrDepartment.doActivity());
@@ -15,6 +29,7 @@ public class Driver {
         doPrintMessage(hrDepartment.getWorkDeadline());
         doPrintMessage(hrDepartment.isTodayAHoliday());
         doPrintMessage("");
+
         TechDepartment techDepartment = new TechDepartment();
         doPrintWelcomeMessage(techDepartment.departmentName());
         doPrintMessage(techDepartment.getTodaysWork());
@@ -24,10 +39,18 @@ public class Driver {
         doPrintMessage("");
     }
 
-    private static void doPrintWelcomeMessage(String departmentName) {
-        System.out.println(new StringBuilder().append("Welcome to").append(" ").append(departmentName).toString());
+    /**
+     * Print method for welcome message.
+     * @param message input message for print.
+     */
+    private static void doPrintWelcomeMessage(String message) {
+        System.out.println(new StringBuilder().append("Welcome to").append(" ").append(message).toString());
     }
 
+    /**
+     * Print method for common message.
+     * @param message input message for print.
+     */
     private static void doPrintMessage(String message) {
         System.out.println(message);
     }
